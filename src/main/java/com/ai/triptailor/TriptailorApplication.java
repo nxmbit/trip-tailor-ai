631538cf1ba6f33ede806ece49d1da8383f40c27
nxmbit <pawelholoownia@gmail.com>
2025-03-25 14:27:13 +0100

    Initial project structure

---
+++ src/main/java/com/ai/triptailor/TriptailorApplication.java
@@ -0,0 +1,13 @@
+package com.ai.triptailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptailorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptailorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/triptailor/TriptailorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.triptailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptailorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
