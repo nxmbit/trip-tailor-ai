016cd93fb3425bee4917e967e983adf0b0b65fa2
nxmbit <pawelholoownia@gmail.com>
2025-05-11 23:58:59 +0200

    Configure environment variables for API base URL and frontend URL in docker-compose.yml

--- frontend/lib/data/api/endpoints.dart
+++ frontend/lib/data/api/endpoints.dart
@@ -1,15 +1,17 @@
+import 'package:frontend/core/config/environment_config.dart';
+
 // This file contains the API constants used in the application.
 class APIConstants {
-  static const String baseUrl = 'http://localhost:8080/';
+  static final String baseUrl = EnvironmentConfig.baseUrl;
 
   // Authentication endpoints
-  static const String registerEndpoint = 'auth/register';
-  static const String loginEndpoint = 'auth/login';
-  static const String refreshTokenEndpoint = 'auth/refresh-token';
-  static const String logoutEndpoint = 'auth/logout';
-  static const String tokensEndpoint = 'auth/tokens';
-  static const String testEndpoint = 'test';
-  static const String oauth2Endpoint = 'oauth2/authorization/';
+  static const String registerEndpoint = '/auth/register';
+  static const String loginEndpoint = '/auth/login';
+  static const String refreshTokenEndpoint = '/auth/refresh-token';
+  static const String logoutEndpoint = '/auth/logout';
+  static const String tokensEndpoint = '/auth/tokens';
+  static const String testEndpoint = '/test';
+  static const String oauth2Endpoint = '/oauth2/authorization/';
 
   // User endpoints
   static const String currentUserEndpoint = '/api/users/profile';
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:frontend/app/app.dart';
 import 'package:frontend/presentation/state/providers/providers_setup.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
   // This ensures Flutter bindings are initialized before using platform channels
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   // Pre-initialize the language provider
   // final languageProvider = LanguageProvider();
   // languageProvider.init();
